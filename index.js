83a7efdca2eb07821aee515480925ad816ef067c
MatthewSingson <80302716+MatthewSingson@users.noreply.github.com>
2022-12-05 22:28:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 
-
+const http = require('http');
+const app =  require('./app');
 
 const port = process.env.PORT || 8080;
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + "/index.html");
-//   homepage
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+  //   homepage
+  })
+  app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+  app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+  app.use('/css/icons', express.static(__dirname + '/node_models/bootstrap-icons/icons'));
+  app.use(express.static('public'));
+  app.listen(port, () => {
+      console.log("Running on port " + port);
 })
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/css/icons', express.static(__dirname + '/node_models/bootstrap-icons/icons'));
-app.use(express.static('public'));
-app.listen(port, () => {
-    console.log("Running on port " + port);
-})
+});
