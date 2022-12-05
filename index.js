bc9b60208cb30bfb23039464a430ac1d59aaec69
MatthewSingson <80302716+MatthewSingson@users.noreply.github.com>
2022-12-05 22:35:10 +0800

    reverted index.js changes

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 
-const http = require('http');
-const app =  require('./app');
+
 
 const port = process.env.PORT || 8080;
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/index.html");
-  //   homepage
-  })
-  app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-  app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-  app.use('/css/icons', express.static(__dirname + '/node_models/bootstrap-icons/icons'));
-  app.use(express.static('public'));
-  app.listen(port, () => {
-      console.log("Running on port " + port);
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+//   homepage
 })
-});
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/css/icons', express.static(__dirname + '/node_models/bootstrap-icons/icons'))
+app.listen(port, () => {
+    console.log("Running on port " + port);
+})
